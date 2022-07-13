ae9c2c173d2b8e7da26f45fd14aeae20bde06437
Alexey Bogoslovskyi <Alexey.b@qualisystems.com>
2022-07-13 21:49:50 +0300

    Fixed isort

--- src/driver.py
+++ src/driver.py
@@ -1,9 +1,9 @@
+from cloudshell.cisco.aci.controller.api.client import CiscoACIControllerHTTPClient
 from cloudshell.core.context.error_handling_context import ErrorHandlingContext
 from cloudshell.devices.driver_helper import get_api, get_logger_with_thread_id
 from cloudshell.shell.core.driver_utils import GlobalLock
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
 
-from cloudshell.cisco.aci.controller.api.client import CiscoACIControllerHTTPClient
 from cisco.aci.physical.configuration_attributes_structure import (
     CiscoACIControllerResourse,
 )
